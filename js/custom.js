d09efc20b64ab04dc68457656d37d86a4c971bb9
fiqih24 <fiqih24@gmail.com>
2018-12-19 14:46:08 +0700

    baru

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,41 @@
 $(function () {
 	"use strict";
-	// typing js part 
+	$(".typed-skill").typed({
+		strings: ["HTML5", "CSS", "Javascript", "PHP","Code Igniter","Laravel","jQuery","Photoshop Basic", "MS.Office","troubleshooting"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 30,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: false,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function () {},
+		// starting callback function before each string
+		preStringTyped: function () {},
+		//callback for every typed string
+		onStringTyped: function () {},
+		// callback for reset
+		resetCallback: function () {}
+	});
+	// typing js part
 	$(".typed").typed({
-		strings: ["OSCAR", "DESIGNER", "DEVELOPER", "CODER"],
+		strings: ["Fiqh Rizki Saputra", "Web Developer", "HIKER", "CODER"],
 		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 		stringsElement: null,
 		// typing speed
@@ -87,7 +120,7 @@ $(function () {
 		$('.navbar-collapse').collapse('hide');
 	});
 
-	// smooth scroll js 
+	// smooth scroll js
 	$(window).scroll(function () {
 		var scrolling = $(this).scrollTop();
 		var stikey = $('.sticky-top');
