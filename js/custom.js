ccf59bd58c335190a7a571fccbd5ed178bf48624
fiqih24 <fiqih24@gmail.com>
2018-12-19 16:33:11 +0700

    baru

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,40 @@
 $(function () {
 	"use strict";
+	$(".nama").typed({
+		strings: ["Fiqh Rizki Saputra"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 30,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: false,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function () {},
+		// starting callback function before each string
+		preStringTyped: function () {},
+		//callback for every typed string
+		onStringTyped: function () {},
+		// callback for reset
+		resetCallback: function () {}
+	});
 	$(".typed-skill").typed({
-		strings: ["HTML5", "CSS", "Javascript", "PHP","Code Igniter","Laravel","jQuery","Photoshop Basic", "MS.Office","troubleshooting"],
+		strings: ["HTML5", "CSS", "Javascript", "PHP","Code Igniter","Laravel","jQuery","Photoshop Basic", "MS.Office","troubleshooting PC"],
 		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 		stringsElement: null,
 		// typing speed
